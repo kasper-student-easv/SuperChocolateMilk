36a4950e63118d746b4aa3f03a1c2b45de1e2ae2
Kasper <kasand04@easv365.dk>
2026-09-14 13:20:26 +0200

    Added MilkRecipeCalculatoe xUnit tests (closes #2)

---
+++ SuperChocolateMilk.Core/MilkRecipeCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace SuperChocolateMilk.Core;
+
+public class MilkRecipeCalculator
+{
+    public static decimal CalculateChocolateSyrupRequired(int milkVolumeMl, String richnessLevel)
+    {
+        if (milkVolumeMl <= 0) return 0m;
+
+        decimal baseSyrupMl = milkVolumeMl * 0.1m;
+
+        return richnessLevel.ToUpper() switch
+        {
+            "LIGHT" => baseSyrupMl * 0.75m,
+            "REGULAR" => baseSyrupMl,
+            "EXTRA" => baseSyrupMl * 1.5m,
+            "ULTRA_CHOCO" => baseSyrupMl * 2.0m,
+            _ => baseSyrupMl
+        };
+    }
+}
---
+++ SuperChocolateMilk.UnitTests/MilkRecipeCalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿namespace SuperChocolateMilk.UnitTests;
+
+using Xunit;
+using SuperChocolateMilk.Core;
+
+public class MilkRecipeCalculatorTests
+{
+    [Fact]
+    public void CalculateChocolateSyrup_RegularRichness_ReturnsTenPercentRatio()
+    {
+        int milkVolum = 1000;
+        string richness = "REGULAR";
+        
+        decimal result = MilkRecipeCalculator.CalculateChocolateSyrupRequired(milkVolum, richness);
+        
+        Assert.Equal(100m, result);
+    }
+
+    [Theory]
+    [InlineData(1000, "LIGHT", 75)]
+    [InlineData(1000, "EXTRA", 150)]
+    [InlineData(1000, "ULTRA_CHOCO", 200)]
+    [InlineData(0, "REGULAR", 0)]
+    public void CalculateChocolateSyrup_VariousScenarios_ReturnsExpectedValue(int milkVolum, string richness, decimal expectedSyrup)
+    {
+        decimal actualResult = MilkRecipeCalculator.CalculateChocolateSyrupRequired(milkVolum, richness);
+        
+        Assert.Equal(expectedSyrup, actualResult);
+    }
+}
